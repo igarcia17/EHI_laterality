c804a3f4ede9a1a67492c2427fee04c7e5727627
igarcia17 <ines.garciaortiz99@gmail.com>
2026-05-22 10:40:28 +0100

    starting with gene expression analyses

--- Part3_PRS/PRS_handedness.R
+++ Part3_PRS/PRS_handedness.R
@@ -11,8 +11,8 @@ workingD <- rstudioapi::getActiveDocumentContext()$path
 setwd(dirname(workingD))
 rm(list = ls())
 ########################Parameters
-cutoffs <- c("0", "40", "60", "80", "90")
-oldies <- TRUE
+cutoffs <- c("0", "60", "90")
+oldies <- T
 write_count_comparisons <- FALSE
 variables <- c("PRS_handedness","PRS_amb","PRS_BD","PRS_SCZ")
 ##########Functions
@@ -47,7 +47,7 @@ create_combined_comparison_table <- function(df, var1, var2){
 ########## Input
 
 input <- as.data.frame(readxl::read_xlsx("All_data_available_PRS.xlsx", sheet = 2, 
-                                         col_types="text", na="#N/A"))%>%
+                                         na="#N/A"))%>%
   mutate(across(
     where(~ n_distinct(., na.rm = TRUE) == 2),
     as.factor
@@ -106,105 +106,222 @@ colMeans(df[variables], na.rm = TRUE)
 apply(df[variables], 2, sd, na.rm = TRUE)
 
 #Visualize
-
 df_long <- df %>%
   pivot_longer(cols = starts_with("PRS"), names_to = "PRS_Type", values_to = "Value") 
+
 # Plot of the 4 PRS in BD patients and controls
-ggplot(df_long, aes(x = Value, fill = STATUS)) +
+ggplot(df_long, aes(x = Value, fill = NRH_0)) +
   geom_density(alpha = 0.5) + 
   facet_wrap(~ PRS_Type, scales = "free_y", ncol = 2) + 
-  scale_fill_manual(values = c("YES" = "#00BFC4", "NO" = "#F8766D")) +
+  #scale_fill_manual(values = c("RH" = "#F8766D","NRH" = "#00BFC4" )) +
   theme_minimal() +
   labs(
-    title = "PRS distribution",
+    #title = "PRS distribution between left and right handers",
     x = "Scaled",
-    y = "Densidad",
-    fill = "BD patient"
+    y = "Densidad"
+    #, fill = "Left (NRH) or right (RH)"
   )
+dev.off()
 
-###Getting into the comparisons
-if(write_count_comparisons){
-pgs_BD <- create_comparison_table(df, "STATUS")
-pgs_hand_cut0 <- create_comparison_table(df, "Cutoff0")
-pgs_hand_cut40<- create_comparison_table(df, "Cutoff40")
-pgs_hand_cut60<- create_comparison_table(df, "Cutoff60")
-pgs_hand_cut80<- create_comparison_table(df, "Cutoff80")
-pgs_hand_cut90<- create_comparison_table(df, "Cutoff90")
-
-wb <- createWorkbook()
-addWorksheet(wb, "all_PGS_all_indiv_byBD")
-addWorksheet(wb, "all_indiv_by_cutoff0")
-addWorksheet(wb, "all_indiv_by_cutoff40")
-addWorksheet(wb, "all_indiv_by_cutoff60")
-addWorksheet(wb, "all_indiv_by_cutoff80")
-addWorksheet(wb, "all_indiv_by_cutoff90")
-
-writeData(wb, "all_PGS_all_indiv_byBD", pgs_BD, rowNames = TRUE)
-writeData(wb, "all_indiv_by_cutoff0", pgs_hand_cut0, rowNames = TRUE)
-writeData(wb, "all_indiv_by_cutoff40", pgs_hand_cut40, rowNames = TRUE)
-writeData(wb, "all_indiv_by_cutoff60", pgs_hand_cut60, rowNames = TRUE)
-writeData(wb, "all_indiv_by_cutoff80", pgs_hand_cut80, rowNames = TRUE)
-writeData(wb, "all_indiv_by_cutoff90", pgs_hand_cut90, rowNames = TRUE)
-
-#PGS only in BD, strata by cutoffs
-df_BD <- df%>% filter(STATUS=="YES") %>%droplevels()
-
-pgs_BD_hand_cut0 <- create_comparison_table(df_BD, "Cutoff0")
-pgs_BD_hand_cut40<- create_comparison_table(df_BD, "Cutoff40")
-pgs_BD_hand_cut60<- create_comparison_table(df_BD, "Cutoff60")
-pgs_BD_hand_cut80<- create_comparison_table(df_BD, "Cutoff80")
-pgs_BD_hand_cut90<- create_comparison_table(df_BD, "Cutoff90")
-
-addWorksheet(wb, "BD_indiv_by_cutoff0")
-addWorksheet(wb, "BD_indiv_by_cutoff40")
-addWorksheet(wb, "BD_indiv_by_cutoff60")
-addWorksheet(wb, "BD_indiv_by_cutoff80")
-addWorksheet(wb, "BD_indiv_by_cutoff90")
-
-writeData(wb, "BD_indiv_by_cutoff0", pgs_BD_hand_cut0, rowNames = TRUE)
-writeData(wb, "BD_indiv_by_cutoff40", pgs_BD_hand_cut40, rowNames = TRUE)
-writeData(wb, "BD_indiv_by_cutoff60", pgs_BD_hand_cut60, rowNames = TRUE)
-writeData(wb, "BD_indiv_by_cutoff80", pgs_BD_hand_cut80, rowNames = TRUE)
-writeData(wb, "BD_indiv_by_cutoff90", pgs_BD_hand_cut90, rowNames = TRUE)
-
-##PGS by seggregating BD and hand type, by cutoff
-comb_0 <- create_combined_comparison_table(df, "STATUS", "Cutoff0")
-comb_40 <- create_combined_comparison_table(df, "STATUS", "Cutoff40")
-comb_60 <- create_combined_comparison_table(df, "STATUS", "Cutoff60")
-comb_80 <- create_combined_comparison_table(df, "STATUS", "Cutoff80")
-comb_90 <- create_combined_comparison_table(df, "STATUS", "Cutoff90")
-
-addWorksheet(wb, "Comb_cutoff0")
-addWorksheet(wb, "Comb_cutoff40")
-addWorksheet(wb, "Comb_cutoff60")
-addWorksheet(wb, "Comb_cutoff80")
-addWorksheet(wb, "Comb_cutoff90")
-
-writeData(wb, "Comb_cutoff0", comb_0, rowNames = TRUE)
-writeData(wb, "Comb_cutoff40", comb_40, rowNames = TRUE)
-writeData(wb, "Comb_cutoff60", comb_60, rowNames = TRUE)
-writeData(wb, "Comb_cutoff80", comb_80, rowNames = TRUE)
-writeData(wb, "Comb_cutoff90", comb_90, rowNames = TRUE)
-
-
-saveWorkbook(wb, "PGS_in_hand_groups_and_BD.xlsx", overwrite = TRUE)
-}
+df_only_BD <- df%>%filter((STATUS=="YES"))
 
 #Run logistic regression
-m1<- glm(NRH_0 ~ SEX + Age + PRS_handedness, data=df, family=binomial)
-m_b<- glm(NRH_0 ~ SEX + Age , data=df, family=binomial)
+m1<- glm(NRH_0 ~ SEX + Age + PC1_oldies+ PC2_oldies + PRS_handedness, data=df,
+         family=binomial
+         )
+m_b<- glm(NRH_0 ~ SEX + Age + PC1_oldies+ PC2_oldies, data=df, family=binomial)
 summary(m1)
 r2_full <-pR2(m1)["r2CU"] ###Full r² (%) Nagelkarke
 r2_base <- pR2(m_b)["r2CU"]
 prs_r2 <- r2_full - r2_base ###PRS r² (%) Nagelkarke
+prs_r2
+r2_full
+
+m1<- lm(Score10items ~ SEX + Age+ PC1_oldies+ PC2_oldies + PRS_handedness, data=df)
+m_b<- lm(Score10items ~ SEX + Age + PC1_oldies+ PC2_oldies, data=df)
+summary(m1)
+summary(m1)$r.squared###Full r² (%) Nagelkarke
+summary(m1)$r.squared - summary(m_b)$r.squared###PRS r² (%) Nagelkarke
+
+m1<- glm(STATUS ~ SEX + Age+ PC1_oldies+ PC2_oldies + PRS_handedness, data=df, family=binomial)
+m_b<- glm(STATUS ~ SEX + Age + PC1_oldies+ PC2_oldies, data=df, family=binomial)
+summary(m1)
+r2_full <-pR2(m1)["r2CU"] ###Full r² (%) Nagelkarke
+r2_base <- pR2(m_b)["r2CU"]
+prs_r2 <- r2_full - r2_base ###PRS r² (%) Nagelkarke
+prs_r2
+r2_full
+
+##Severity
+m1<- lm(WHODAS ~ SEX + Age+ PC1_oldies+ PC2_oldies + PRS_handedness, data=df_only_BD)
+m_b<- lm(WHODAS ~ SEX + Age + PC1_oldies+ PC2_oldies, data=df_only_BD)
+summary(m1)
+summary(m1)$r.squared
+summary(m1)$r.squared - summary(m_b)$r.squared
+
+m1<- lm(CGI ~ SEX + Age+ PC1_oldies+ PC2_oldies + PRS_handedness, data=df_only_BD)
+m_b<- lm(CGI ~ SEX + Age + PC1_oldies+ PC2_oldies, data=df_only_BD)
+summary(m1)
+summary(m1)$r.squared###Full r² (%) Nagelkarke
+summary(m1)$r.squared - summary(m_b)$r.squared###PRS r² (%) Nagelkarke
+
+m1<- lm(INES ~ SEX + Age+ PC1_oldies+ PC2_oldies + PRS_handedness, data=df_only_BD)
+m_b<- lm(INES ~ SEX + Age + PC1_oldies+ PC2_oldies, data=df_only_BD)
+summary(m1)
+summary(m1)$r.squared###Full r² (%) Nagelkarke
+summary(m1)$r.squared - summary(m_b)$r.squared###PRS r² (%) Nagelkarke
+
+m1<- lm(GAF ~ SEX + Age+ PC1_oldies+ PC2_oldies + PRS_handedness, data=df_only_BD)
+m_b<- lm(GAF ~ SEX + Age + PC1_oldies+ PC2_oldies, data=df_only_BD)
+summary(m1)
+summary(m1)$r.squared###Full r² (%) Nagelkarke
+summary(m1)$r.squared - summary(m_b)$r.squared###PRS r² (%) Nagelkarke
+
+###PRS of BD
+m1<- glm(STATUS ~ SEX + Age+ PC1_oldies+ PC2_oldies + PRS_BD, data=df, family=binomial)
+m_b<- glm(STATUS ~ SEX + Age + PC1_oldies+ PC2_oldies, data=df, family=binomial)
+summary(m1)
+r2_full <-pR2(m1)["r2CU"] ###Full r² (%) Nagelkarke
+r2_base <- pR2(m_b)["r2CU"]
+prs_r2 <- r2_full - r2_base ###PRS r² (%) Nagelkarke
+prs_r2
+r2_full
+
+##Severity using PRS of BD
+m1<- lm(WHODAS ~ SEX + Age+ PC1_oldies+ PC2_oldies + PRS_BD, data=df_only_BD)
+m_b<- lm(WHODAS ~ SEX + Age + PC1_oldies+ PC2_oldies, data=df_only_BD)
+summary(m1)
+summary(m1)$r.squared
+summary(m1)$r.squared - summary(m_b)$r.squared
+
+m1<- lm(CGI ~ SEX + Age+ PC1_oldies+ PC2_oldies + PRS_BD, data=df_only_BD)
+m_b<- lm(CGI ~ SEX + Age + PC1_oldies+ PC2_oldies, data=df_only_BD)
+summary(m1)
+summary(m1)$r.squared###Full r² (%) Nagelkarke
+summary(m1)$r.squared - summary(m_b)$r.squared###PRS r² (%) Nagelkarke
+
+m1<- lm(INES ~ SEX + Age+ PC1_oldies+ PC2_oldies + PRS_BD, data=df_only_BD)
+m_b<- lm(INES ~ SEX + Age + PC1_oldies+ PC2_oldies, data=df_only_BD)
+summary(m1)
+summary(m1)$r.squared###Full r² (%) Nagelkarke
+summary(m1)$r.squared - summary(m_b)$r.squared###PRS r² (%) Nagelkarke
+
+m1<- lm(GAF ~ SEX + Age+ PC1_oldies+ PC2_oldies + PRS_BD, data=df_only_BD)
+m_b<- lm(GAF ~ SEX + Age + PC1_oldies+ PC2_oldies, data=df_only_BD)
+summary(m1)
+summary(m1)$r.squared###Full r² (%) Nagelkarke
+summary(m1)$r.squared - summary(m_b)$r.squared###PRS r² (%) Nagelkarke
+
+#Left vs right predicted by PRS of BD in all samples and only in cases
+m1<- glm(NRH_0 ~ SEX + Age+ PC1_oldies+ PC2_oldies + PRS_BD, data=df, family=binomial)
+m_b<- glm(NRH_0 ~ SEX + Age + PC1_oldies+ PC2_oldies, data=df, family=binomial)
+summary(m1)
+r2_full <-pR2(m1)["r2CU"] ###Full r² (%) Nagelkarke
+r2_base <- pR2(m_b)["r2CU"]
+prs_r2 <- r2_full - r2_base ###PRS r² (%) Nagelkarke
+prs_r2
+r2_full
+
+m1<- glm(NRH_0 ~ SEX + Age+ PC1_oldies+ PC2_oldies + PRS_BD, data=df_only_BD, family=binomial)
+m_b<- glm(NRH_0 ~ SEX + Age + PC1_oldies+ PC2_oldies, data=df_only_BD, family=binomial)
+summary(m1)
+r2_full <-pR2(m1)["r2CU"] ###Full r² (%) Nagelkarke
+r2_base <- pR2(m_b)["r2CU"]
+prs_r2 <- r2_full - r2_base ###PRS r² (%) Nagelkarke
+prs_r2
+r2_full
+
+m1<- lm(Score10items ~ SEX + Age+ PC1_oldies+ PC2_oldies + PRS_BD, data=df)
+m_b<- lm(Score10items ~ SEX + Age + PC1_oldies+ PC2_oldies, data=df)
+summary(m1)
+summary(m1)$r.squared###Full r² (%) Nagelkarke
+summary(m1)$r.squared - summary(m_b)$r.squared###PRS r² (%) Nagelkarke
+
+m1<- lm(Score10items ~ SEX + Age+ PC1_oldies+ PC2_oldies + PRS_BD, data=df_only_BD)
+m_b<- lm(Score10items ~ SEX + Age + PC1_oldies+ PC2_oldies, data=df_only_BD)
+summary(m1)
+summary(m1)$r.squared###Full r² (%) Nagelkarke
+summary(m1)$r.squared - summary(m_b)$r.squared###PRS r² (%) Nagelkarke
 
 
-summary(glm(Score10items ~ SEX + Age + PRS_handedness, data=df
-         #, family=binomial
-         ))
+
+
+
+
+
 
 
 #
 #
 #
 
+###Getting into the comparisons
+if(write_count_comparisons){
+  pgs_BD <- create_comparison_table(df, "STATUS")
+  pgs_hand_cut0 <- create_comparison_table(df, "Cutoff0")
+  pgs_hand_cut40<- create_comparison_table(df, "Cutoff40")
+  pgs_hand_cut60<- create_comparison_table(df, "Cutoff60")
+  pgs_hand_cut80<- create_comparison_table(df, "Cutoff80")
+  pgs_hand_cut90<- create_comparison_table(df, "Cutoff90")
+  
+  wb <- createWorkbook()
+  addWorksheet(wb, "all_PGS_all_indiv_byBD")
+  addWorksheet(wb, "all_indiv_by_cutoff0")
+  addWorksheet(wb, "all_indiv_by_cutoff40")
+  addWorksheet(wb, "all_indiv_by_cutoff60")
+  addWorksheet(wb, "all_indiv_by_cutoff80")
+  addWorksheet(wb, "all_indiv_by_cutoff90")
+  
+  writeData(wb, "all_PGS_all_indiv_byBD", pgs_BD, rowNames = TRUE)
+  writeData(wb, "all_indiv_by_cutoff0", pgs_hand_cut0, rowNames = TRUE)
+  writeData(wb, "all_indiv_by_cutoff40", pgs_hand_cut40, rowNames = TRUE)
+  writeData(wb, "all_indiv_by_cutoff60", pgs_hand_cut60, rowNames = TRUE)
+  writeData(wb, "all_indiv_by_cutoff80", pgs_hand_cut80, rowNames = TRUE)
+  writeData(wb, "all_indiv_by_cutoff90", pgs_hand_cut90, rowNames = TRUE)
+  
+  #PGS only in BD, strata by cutoffs
+  df_BD <- df%>% filter(STATUS=="YES") %>%droplevels()
+  
+  pgs_BD_hand_cut0 <- create_comparison_table(df_BD, "Cutoff0")
+  pgs_BD_hand_cut40<- create_comparison_table(df_BD, "Cutoff40")
+  pgs_BD_hand_cut60<- create_comparison_table(df_BD, "Cutoff60")
+  pgs_BD_hand_cut80<- create_comparison_table(df_BD, "Cutoff80")
+  pgs_BD_hand_cut90<- create_comparison_table(df_BD, "Cutoff90")
+  
+  addWorksheet(wb, "BD_indiv_by_cutoff0")
+  addWorksheet(wb, "BD_indiv_by_cutoff40")
+  addWorksheet(wb, "BD_indiv_by_cutoff60")
+  addWorksheet(wb, "BD_indiv_by_cutoff80")
+  addWorksheet(wb, "BD_indiv_by_cutoff90")
+  
+  writeData(wb, "BD_indiv_by_cutoff0", pgs_BD_hand_cut0, rowNames = TRUE)
+  writeData(wb, "BD_indiv_by_cutoff40", pgs_BD_hand_cut40, rowNames = TRUE)
+  writeData(wb, "BD_indiv_by_cutoff60", pgs_BD_hand_cut60, rowNames = TRUE)
+  writeData(wb, "BD_indiv_by_cutoff80", pgs_BD_hand_cut80, rowNames = TRUE)
+  writeData(wb, "BD_indiv_by_cutoff90", pgs_BD_hand_cut90, rowNames = TRUE)
+  
+  ##PGS by seggregating BD and hand type, by cutoff
+  comb_0 <- create_combined_comparison_table(df, "STATUS", "Cutoff0")
+  comb_40 <- create_combined_comparison_table(df, "STATUS", "Cutoff40")
+  comb_60 <- create_combined_comparison_table(df, "STATUS", "Cutoff60")
+  comb_80 <- create_combined_comparison_table(df, "STATUS", "Cutoff80")
+  comb_90 <- create_combined_comparison_table(df, "STATUS", "Cutoff90")
+  
+  addWorksheet(wb, "Comb_cutoff0")
+  addWorksheet(wb, "Comb_cutoff40")
+  addWorksheet(wb, "Comb_cutoff60")
+  addWorksheet(wb, "Comb_cutoff80")
+  addWorksheet(wb, "Comb_cutoff90")
+  
+  writeData(wb, "Comb_cutoff0", comb_0, rowNames = TRUE)
+  writeData(wb, "Comb_cutoff40", comb_40, rowNames = TRUE)
+  writeData(wb, "Comb_cutoff60", comb_60, rowNames = TRUE)
+  writeData(wb, "Comb_cutoff80", comb_80, rowNames = TRUE)
+  writeData(wb, "Comb_cutoff90", comb_90, rowNames = TRUE)
+  
+  
+  saveWorkbook(wb, "PGS_in_hand_groups_and_BD.xlsx", overwrite = TRUE)
+}
+#
+#
+#
